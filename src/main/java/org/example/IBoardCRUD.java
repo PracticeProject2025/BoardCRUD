42d36cb6471a27348a4f57c58841486889222cad
hayoung <kanghayoung527@gmail.com>
2025-11-09 18:08:04 +0900

    fix Interface field

--- src/main/java/org/example/IBoardCRUD.java
+++ src/main/java/org/example/IBoardCRUD.java
@@ -1,4 +1,11 @@
 package org.example;
 
-public class IBoardCRUD {
+public interface IBoardCRUD {
+    public void addItem();
+    public void listAll();
+    public void updateItem();
+    public void deleteItem();
+    public void searchTitle();
+    public void searchAuthor();
+    public void viewRanking();
 }
